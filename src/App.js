847408b0e9f7748119f30622ae8ebcc71d48b916
爱学兮 <aixuexi567@icloud.com>
2024-04-10 19:39:06 +0800

    Pizza Component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// App components
+export function App() {
+  const a = '22';
+  return (
+    <>
+      <h1>Hello World!!</h1>
+      <p>Jiang</p>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom/client';
 
 // App components
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World!!</h1>
+      <Pizza />
+    </>
+  );
+}
+function Pizza() {
+  return (
+    <>
+      <h1>Hello Pizza!!</h1>
+      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+    </>
+  );
 }
 // React V18
 const root = ReactDOM.createRoot(document.getElementById('root'));
